bb769c779f16c35b2020ff2eb817bda89463c927
jagercha000 <jagercha000@stillwaterschools.org>
2026-05-12 17:37:30 -0500

    Update header.js

--- scripts/header.js
+++ scripts/header.js
@@ -1,1 +1,7 @@
-
+globalThis.hooks['header'] = function() {
+  document.querySelectorAll('.header-menu-dropdown-button').forEach(function(element) {
+    element.addEventListener('click', function(evt) {
+      evt.preventDefault();
+    });
+  });
+};
