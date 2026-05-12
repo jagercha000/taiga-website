8062fc3950e810eda388de2b5d77c7437026aa65
jagercha000 <jagercha000@stillwaterschools.org>
2026-05-12 18:16:42 -0500

    Update header.js

--- scripts/header.js
+++ scripts/header.js
@@ -1,8 +1,17 @@
 globalThis.hooks = globalThis.hooks || new Object();
+function hideHeaderDropdowns() {
+  document.querySelectorAll('.header-menu-dropdown-content').forEach(function(element) {
+    element.classList.add('hidden');
+  });
+}
 globalThis.hooks['header-init'] = function() {
   document.querySelectorAll('.header-menu-dropdown-button').forEach(function(element) {
     element.addEventListener('click', function(evt) {
       evt.preventDefault();
+      evt.target.parentElement.querySelector('.header-menu-dropdown-content').classList.remove('hidden');
     });
+  });
+  document.body.addEventListener('click', function(evt) {
+    hideHeaderDropdowns();
   });
 };
