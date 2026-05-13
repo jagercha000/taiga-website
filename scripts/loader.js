c7563afb45b4b37200ad715e24a4b7da17711479
jagercha000 <jagercha000@stillwaterschools.org>
2026-05-12 20:42:12 -0500

    Update loader.js

--- scripts/loader.js
+++ scripts/loader.js
@@ -1,3 +1,9 @@
 globalThis.hooks = globalThis.hooks || new Object();
 globalThis.hooks['loader-init'] = function() {};
-globalThis.hooks['loader-hide'] = function() {};
+globalThis.hooks['loader-hide'] = function() {
+  setTimeout(function() {
+    gsap.fromTo('.loader-overlay', { opacity: 0 }, { opacity: 1, duration: 2, onComplete: function() {
+      document.querySelector('.loader-overlay').classList.add('hidden');
+    }});
+  }, 2500);
+);};
