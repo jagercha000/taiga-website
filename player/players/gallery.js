294e2058dbedbe7f70112f4cc5787d5fb1625cf6
jagercha000 <jagercha000@stillwaterschools.org>
2026-05-13 12:25:14 -0500

    Update gallery.js

--- player/players/gallery.js
+++ player/players/gallery.js
@@ -1,9 +1,9 @@
 async function fetchPictureMetadata() {
-  var result = await fetch('../../assets/gallery/pictures.json');
+  var result = await fetch('../assets/gallery/pictures.json');
   return await result.json();
 }
 async function downloadPicture(pictureObject) {
-  var result = await fetch('../../assets/gallery/' + pictureObject.url);
+  var result = await fetch('../assets/gallery/' + pictureObject.url);
   var buf = await result.arrayBuffer();
   var blob = new Blob([buf], { 'type': pictureObject.type });
   return URL.createObjectURL(blob);
