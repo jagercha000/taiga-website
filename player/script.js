7f05d97c1705ea8b928fb2f999cbd7a68497e025
jagercha000 <jagercha000@stillwaterschools.org>
2026-05-13 12:31:09 -0500

    Update script.js

--- player/script.js
+++ player/script.js
@@ -3,10 +3,16 @@ window.addEventListener('load', function() {
   var urlParams = new URLSearchParams(window.location.search);
   if(urlParams.get('id')) {
     var playerScript = "./players/" + urlParams.get('id') + ".js";
+    var playerStyle = "./players/" + urlParams.get('id') + ".css";
     var element = document.createElement('script');
     element.setAttribute('type', 'module');
     element.setAttribute('src', playerScript);
     document.head.appendChild(element);
+    var element2 = document.createElement('link');
+    element2.setAttribute('rel', 'stylesheet');
+    element2.setAttribute('type', 'text/css');
+    element2.setAttribute('src', playerStyle);
+    document.head.appendChild(element2);
   }
   var playerCanvas = document.querySelector('.player-canvas');
   function updatePlayerCanvasSize() {
