d330cf17a53043fcaeca5e297436c933e14ada61
Charlie000 <jagercha000@stillwaterschools.org>
2026-05-14 08:44:08 -0500

    Update climate.js

--- player/players/climate.js
+++ player/players/climate.js
@@ -1,1 +1,10 @@
-
+globalThis.player.climateData = globalThis.player.climateData || new Object();
+globalThis.player.climateData.images = globalThis.player.climateData.images || new Object();
+window.addEventListener('load', async function() {
+  var element = document.createElement('div');
+  element.setAttribute('class', 'climate-loading');
+  element.innerText = "Loading";
+  document.body.appendChild(element);
+  globalThis.player.climateData.images.winter = await globalThis.player.utils.downloadImage("climate/winter.jpg");
+  globalThis.player.climateData.images.summer = await globalThis.player.utils.downloadImage("climate/summer.jpg");
+});
