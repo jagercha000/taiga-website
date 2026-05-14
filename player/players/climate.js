7ea4f7184af138b3d4619aece072369d7493a13d
Charlie000 <jagercha000@stillwaterschools.org>
2026-05-14 09:36:14 -0500

    Update climate.js

--- player/players/climate.js
+++ player/players/climate.js
@@ -4,14 +4,20 @@ var element = document.createElement('div');
 element.setAttribute('class', 'climate-loading');
 element.innerText = "Loading";
 document.body.appendChild(element);
-globalThis.player.climateData.images.winter = await globalThis.player.util.downloadImage("climate/winter.jpg");
-globalThis.player.climateData.images.summer = await globalThis.player.util.downloadImage("climate/summer.jpg");
+async function registerImage(id, url) {
+  globalThis.player.climateData.images[id] = new Object();
+  globalThis.player.climateData.images[id].url = await globalThis.player.util.downloadImage(url);
+  globalThis.player.climateData.images[id].image = new Image();
+  globalThis.player.climateData.images[id].image.src = globalThis.player.climateData.images[id].url;
+}
+await registerImage("winter", "climate/winter.jpg");
+await registerImage("summer", "climate/summer.jpg");
 globalThis.player.climateData.currentSeason = "winter";
 async function climateFrame() {
   if(globalThis.player.climateData.currentSeason == "winter") {
-    globalThis.player.util.fitImage(globalThis.player.climateData.images.winter);
+    globalThis.player.util.fitImage(globalThis.player.climateData.images.winter.image);
   } else if(globalThis.player.climateData.currentSeason == "summer") {
-    globalThis.player.util.fitImage(globalThis.player.climateData.images.summer);
+    globalThis.player.util.fitImage(globalThis.player.climateData.images.summer.image);
   }
   window.requestAnimationFrame(climateFrame);
 }
