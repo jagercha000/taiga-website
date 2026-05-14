f91fb987a11721451e97448cb917409acbdc5d59
Charlie000 <jagercha000@stillwaterschools.org>
2026-05-14 12:09:20 -0500

    Update error.js

--- scripts/error.js
+++ scripts/error.js
@@ -1,1 +1,9 @@
-
+import "./constants.js"
+window.addEventListener('load', function() {
+  document.querySelector('.error-back-button').addEventListener('click', function() {
+    window.history.go(-1);
+  });
+  document.querySelector('.error-home-button').addEventListener('click', function() {
+    window.location.href = sitePath;
+  });
+});
