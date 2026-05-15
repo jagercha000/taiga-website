7c374bdd4e7856dd8e642bc0133b17c8ebfaa18a
Charlie000 <jagercha000@stillwaterschools.org>
2026-05-15 14:21:15 -0500

    Update predators.js

--- player/players/predators.js
+++ player/players/predators.js
@@ -1,1 +1,11 @@
-
+globalThis.player.predatorsData = globalThis.player.predatorsData || new Object();
+globalThis.player.predatorsData.background = new Object();
+globalThis.player.predatorsData.background.url = await globalThis.player.util.downloadImage('predator-prey/predators.jpg');
+globalThis.player.predatorsData.background.image = new Image();
+globalThis.player.predatorsData.background.image.src = globalThis.player.predatorsData.background.url;
+function predatorsFrame() {
+  globalThis.player.util.fitImage(globalThis.player.predatorsData.background.image);
+  window.requestAnimationFrmae(predatorsFrame);
+}
+window.requestAnimationFrame(predatorsFrame);
+globalThis.player.stopLoading();
